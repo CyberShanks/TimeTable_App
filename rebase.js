ed25005fe76dab2c8d58b35684ca8e8836fbe242
Shashank <shashankmish27@gmail.com>
2023-01-27 23:42:17 +0530

    Drag Event Fully Functional Highlight still broken

--- rebase.js
+++ rebase.js
@@ -5,12 +5,15 @@ function createNewTimeEntry(type) {
     newTimeEntry.setAttribute("draggable", "true");
     newTimeEntry.setAttribute("ondrop", "drop_handler(event)");
     newTimeEntry.setAttribute("ondragover", "dragover_handler(event)");
+    newTimeEntry.addEventListener("dragstart", timeEntryDragHandler);
+
     let newTime = document.createElement('div');
     let timeClass = String(type) + " time";
     newTime.setAttribute("class", timeClass);
     // if one or multi, also add click event
-    if (type == "one" || type == "multi"){
+    if (type == "one" || type == "multi") {
         newTime.addEventListener('click', oneHourClickHandler);
+        console.log("click event added");
     }
 
     let newAction = document.createElement('div');
@@ -36,8 +39,74 @@ function checkHalf(part1, part2) {
     else return 3;
 }
 
+//pointers to modal-window and page-1
+const modal = document.querySelector("#modal-window");
+const page1 = document.querySelector("#page-1");
+
+//show ModalWindow and minimize page1
+function displayModalWindow() {
+    modal.classList.remove("hide-modal");
+    modal.classList.add("show-modal");
+    page1.classList.remove("show-page1");
+    page1.classList.add("hide-page1");
+}
+
+//minimize ModalWindow and show page1
+function hideModalWindow(e) {
+    modal.classList.remove("show-modal");
+    modal.classList.add("hide-modal");
+    page1.classList.remove("hide-page1");
+    page1.classList.add("show-page1");
+}
+
+function resetBorders() {
+    timeEntries.forEach(timeEntry => timeEntry.style.border = '');
+}
+
+//////////////////////////////////////////
+
+//deletes time-entry divs, from target to dragged index, in the direction provided
+function divCleaner(targetIndex, draggedIndex, direction){
+    if (direction == "up"){
+        for (let index = draggedIndex; index > targetIndex; index--){
+            container.removeChild(container.children[index])
+        }
+    }
+    else if (direction == "down"){
+        for (let index = targetIndex-1; index >= draggedIndex; index--){
+            container.removeChild(container.children[index])
+        }
+}}
+
+// add class to target div
+function classAdder(index){
+    //fetch time parts to compare
+    let timeString1 = fetchElementTimePart(container.children[index].firstElementChild, 1);
+    let timeString2 = fetchElementTimePart(container.children[index].firstElementChild, 2);
+
+    time1 = parseInt(timeString1.slice(0,2));
+    time2 = parseInt(timeString2.slice(0,2));
+
+    //check if hhour difference bigger than 1
+    if (time2 - time1 > 1){
+        container.children[index].firstElementChild.classList = "multi time";
+        container.children[index].firstElementChild.removeEventListener("click", oneHourClickHandler);
+    }
+    else {
+        container.children[index].firstElementChild.classList = "one time";
+        container.children[index].firstElementChild.addEventListener('click', oneHourClickHandler);
+
+    }
+
+    //if time is XX:00-YY:30, its half MULTI/ONE
+    if (checkHalf(timeString1, timeString2) == 1 || checkHalf(timeString1, timeString2) == 2){
+        container.children[index].firstElementChild.classList.add("half");
+    }
+}
 
 /*Events*/
+
+/*Event Handler Functions */
 
 //oneHour Click Event Handler function
 function oneHourClickHandler(e) {
@@ -53,11 +122,11 @@ function oneHourClickHandler(e) {
         if (halfPart == 1) {
             //set time for new TimeEntry
             let hour = firstPart.slice(0, 2);
-            let newTimeString = String(parseInt(hour)+1) + ":00-" + secondPart;
+            let newTimeString = String(parseInt(hour) + 1) + ":00-" + secondPart;
             newTimeEntry.firstElementChild.textContent = newTimeString;
 
             //update existing Time-Entry time range
-            let updatedTimeString = firstPart + "-" + String(parseInt(hour)+1) + ":00";
+            let updatedTimeString = firstPart + "-" + String(parseInt(hour) + 1) + ":00";
             e.target.textContent = updatedTimeString;
 
             //insert below target
@@ -66,11 +135,11 @@ function oneHourClickHandler(e) {
         else if (halfPart == 2) {
             //set time for new TimeEntry
             let hour = firstPart.slice(0, 2);
-            let newTimeString = firstPart + "-" + String(parseInt(hour)+1) + ":00";
-            newTimeEntry.firstElementChild.textContent = newTimeString;      
+            let newTimeString = firstPart + "-" + String(parseInt(hour) + 1) + ":00";
+            newTimeEntry.firstElementChild.textContent = newTimeString;
 
             //update existing Time-Entry time range
-            let updatedTimeString = String(parseInt(hour)+1) + ":00-" + secondPart;
+            let updatedTimeString = String(parseInt(hour) + 1) + ":00-" + secondPart;
             e.target.textContent = updatedTimeString;
 
             //insert above target
@@ -100,8 +169,137 @@ function oneHourClickHandler(e) {
     e.stopPropagation();
 }
 
-// one hour time-entry click event 
+
+// action Click Event Handler Code 
+//store which element was clicked on 
+let actionNode;
+
+function actionClickHandler(e) {
+    //check to see if target has action class and is variable
+    if ((e.target).classList == 'action' && ((e.target).parentElement).classList.contains('variable')) {
+        displayModalWindow();
+        actionNode = e.target;
+    }
+}
+
+// option Click Event Handler Function
+function optionClickHandler(e) {
+    hideModalWindow();
+    chosenOption = e.target.textContent.trim();
+    console.log(chosenOption);
+    actionNode.textContent = chosenOption;
+}
+
+// time-entry Drag Event Handler <<Initialization>> Function
+function timeEntryDragHandler(e){
+    //get and store time text by part
+    let timeDraggedpart1 = fetchElementTimePart(e.target.firstElementChild, 1);
+    let timeDraggedpart2 = fetchElementTimePart(e.target.firstElementChild, 2);
+    console.log(e.target);
+    console.log(timeDraggedpart1);
+    console.log(timeDraggedpart2);
+    e.dataTransfer.setData("text/time1", timeDraggedpart1);
+    e.dataTransfer.setData("text/time2", timeDraggedpart2);
+
+    //get and store child node number for deletion arithmetic
+    let draggedNodeChildIndex = Array.prototype.indexOf.call(container.children, e.target);
+    e.dataTransfer.setData("text/node", draggedNodeChildIndex);
+}
+
+// time-entry DRAGOVER Event Handler Function 
+function dragover_handler(e) {
+    //prevent default dragover action
+    e.preventDefault();
+    
+    // making sure that time-entry is selected as target
+    let target = e.target;
+    if (e.target.classList.contains('time') || e.target.classList.contains('action')){
+        target = e.target.parentElement;
+    }
+    // highlight effect
+    e.dataTransfer.dropEffect = "move";
+    target.style.border = '2px solid orange';
+}
+
+// time-entry Drop Event Handler Function
+function drop_handler(e) {
+    e.preventDefault();
+    //make sure target is time-entry div
+    let target = e.target;
+    if (e.target.classList.contains('time') || e.target.classList.contains('action')){
+        target = e.target.parentElement;
+    }
+    //fetch data
+    let draggedNodeChildIndex = e.dataTransfer.getData("text/node");
+    let draggedTime1 = e.dataTransfer.getData("text/time1");
+    let draggedTime2 = e.dataTransfer.getData("text/time2");
+    console.log(draggedTime1);
+    console.log(draggedTime2);
+
+    //find required vars
+    let targetNodeChildIndex = Array.prototype.indexOf.call(container.children, target);
+    console.log(targetNodeChildIndex);
+    targetTime1 = fetchElementTimePart(target.firstElementChild, 1);
+    targetTime2 = fetchElementTimePart(target.firstElementChild, 2);
+
+    //if dragged up
+    if (draggedNodeChildIndex > targetNodeChildIndex) {
+        //time becomes -> target:hour => 1st part of target time + 2nd part of dragged Element time
+        newTimeString = targetHour1 + '-' + draggedTime2;
+        divCleaner(targetNodeChildIndex, draggedNodeChildIndex, "up");
+    }
+
+    //if dragged down
+    else if (draggedNodeChildIndex < targetNodeChildIndex) {
+        //time becomes -> dragged:target => 1st part of dragged time + 2nd part of target time
+        newTimeString = draggedTime1 + '-' + targetTime2;
+        divCleaner(targetNodeChildIndex, draggedNodeChildIndex, "down");
+    }
+
+    //if drag to same place, do nothing
+    else {
+        return;
+    }
+
+    targetNodeChildIndex = Array.prototype.indexOf.call(container.children, target);
+    target.firstElementChild.textContent = newTimeString;
+    
+    //change class of target element
+    classAdder(targetNodeChildIndex);
+
+    //RESET BORDERS BROKEN
+    resetBorders();
+    target.style.border = ''
+}
+
+/*Event Listeners */
+
+// one hour --Time-Entry-- click event 
 let oneHours = document.querySelectorAll(".one");
 oneHours.forEach(oneHour => {
     oneHour.addEventListener('click', oneHourClickHandler);
+});
+
+// bubble up --Action-- Click Event
+let container = document.querySelector("#container");
+container.addEventListener("click", actionClickHandler);
+
+// --svg-- Click Event
+let svg = document.querySelector("svg");
+svg.addEventListener("click", hideModalWindow);
+
+// --options-- Click Event
+let options = document.querySelectorAll(".options");
+options.forEach(option => {
+    option.addEventListener("click", optionClickHandler);
+});
+
+// --TimeEntry-- Drag Event 
+let timeEntries = document.querySelectorAll(".variable");
+timeEntries.forEach(timeEntry => {
+    // make every variable time-entry element draggable and drop target
+    timeEntry.setAttribute("draggable", "true");
+    timeEntry.setAttribute("ondrop", "drop_handler(event)");
+    timeEntry.setAttribute("ondragover", "dragover_handler(event)");
+    timeEntry.addEventListener("dragstart", timeEntryDragHandler);
 });
